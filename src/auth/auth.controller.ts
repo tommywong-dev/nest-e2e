e70d83913783986849528a4669cbfebb3a9f2b7c
Tommy Wong <solsedu@sols247.org>
2022-04-11 00:33:34 +0800

    init

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthDto } from 'src/dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    console.log({ dto });
+    return this.authService.signup(dto);
+  }
+
+  @Post('signin')
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,55 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
+import { AuthDto } from 'src/dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    try {
+      const hash = await argon.hash(dto.password);
+
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email is taken');
+        }
+      }
+      throw error;
+    }
+  }
+
+  async signin(dto: AuthDto) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+
+      if (!user) throw new ForbiddenException('Credentials incorrect');
+
+      const passwordVerified = await argon.verify(user.hash, dto.password);
+
+      if (!passwordVerified)
+        throw new ForbiddenException('Credentials incorrect');
+
+      delete user.hash;
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
